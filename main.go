881af7d28af524bc0bdd178cbbcbfe5c2140f8db
develop <dofastsite@gmail.com>
2024-10-29 02:00:19 +0300

    Swagger work

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-  "net/http"
-  "github.com/gin-gonic/gin"
+    "log"
+
+    _ "go-rest-api/docs"
+
+    "github.com/gin-contrib/cors"
+    "github.com/gin-gonic/gin"
+    swaggerFiles "github.com/swaggo/files"
+    ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
+    router := gin.Default()
+    router.Use(cors.Default())
+    router.GET("swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	r.GET("/", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{"data": "hello world"})    
-		})
-	r.Run() 
+    err := router.Run("localhost:8080")
+    if err != nil {
+        log.Println("Error running router: ", err)
+    }
+
 }
